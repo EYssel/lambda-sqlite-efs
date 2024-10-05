cca2f1b2158abe624630609f955fa05f9dd05b42
Estian Yssel <38258302+EYssel@users.noreply.github.com>
2024-10-05 21:32:01 +0200

    feat: return response

--- lib/lambda/index.ts
+++ lib/lambda/index.ts
@@ -8,7 +8,7 @@ const { db } = bootstrap();
 export const handler = async (event: ApiEvent, ctx: ApiContext) => {
     ctx.db = db;
 
-    routeHandlers[event.route](event.data, ctx);
+    return routeHandlers[event.route](event.data, ctx);
 };
 
 const routeHandlers: Record<Route, (event: ApiEvent, ctx: ApiContext) => any> = {
